5adb02c0acf7ead4af859d34b114119c82db0897
lfp2 <lfp2@cin.ufpe.br>
2020-06-14 19:52:58 -0300

    Making the first sign up screen

---
+++ app/src/pages/Cadastro/firstScreen.js
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+import Header from '../../components/Header';
+import {
+  Container,
+  HeaderText,
+  Button,
+  ProfilePic,
+  TruckerName,
+  TruckerPic,
+  TransportadoraPic,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+export default function FirstScreen() {
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <Header title="Cadastro" />
+      <ProfilePic />
+      <TruckerName>Ednaldo Pereira</TruckerName>
+      <TruckerPic />
+      <TransportadoraPic />
+      <HeaderText>É você?</HeaderText>
+      <Button isPrimary={true}>Sim, Tks</Button>
+      <Button>Não</Button>
+    </Container>
+  );
+}
